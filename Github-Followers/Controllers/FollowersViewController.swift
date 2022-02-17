2156a5dd6ad79aa14ead5c2d0f5b976ef28a3f5a
Zeljko Lucic <zeljko.lucic99@gmail.com>
2022-02-17 10:38:09 +0100

    Result type refactor

--- Github-Followers/Controllers/FollowersViewController.swift
+++ Github-Followers/Controllers/FollowersViewController.swift
@@ -36,15 +36,13 @@ class FollowersViewController: UIViewController {
             return
         }
         
-        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, error in
-            guard let followers = followers else {
-                self.presentGFAlertOnMainThread(title: "Error", message: error!.rawValue, buttonTitle: "Ok")
-                return
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            switch result {
+            case .success(let followers):
+                print(followers)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Error", message: error.rawValue, buttonTitle: "Ok")
             }
-
-            print("Followers.count = \(followers.count)")
-            print(followers)
-            
         }
     }
     
--- Github-Followers/Managers/NetworkManager.swift
+++ Github-Followers/Managers/NetworkManager.swift
@@ -19,27 +19,27 @@ class NetworkManager {
     
     private init() {}
     
-    func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, ErrorMessage?) -> Void) {
+    func getFollowers(for username: String, page: Int, completionHandler: @escaping (Result<[Follower], GFError>) -> Void) {
         let endpoint = "\(baseURL)/users/\(username)/followers?per_page=\(perPage)&page=\(page)"
         
         guard let url = URL(string: endpoint) else {
-            completed(nil, .invalidUsername)
+            completionHandler(.failure(.invalidUsername))
             return
         }
         
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
             if let _ = error {
-                completed(nil, .unableToComplete)
+                completionHandler(.failure(.unableToComplete))
                 return
             }
             
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completed(nil, .invalidResponse)
+                completionHandler(.failure(.invalidResponse))
                 return
             }
             
             guard let data = data else {
-                completed(nil, .invalidData)
+                completionHandler(.failure(.invalidData) )
                 return
             }
             
@@ -47,9 +47,9 @@ class NetworkManager {
                 let decoder = JSONDecoder()
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
                 let followers = try decoder.decode([Follower].self, from: data)
-                completed(followers, nil)
+                completionHandler(.success(followers))
             } catch {
-                completed(nil, .invalidData)
+                completionHandler(.failure(.invalidData))
             }
 
         }
